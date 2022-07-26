e020a6b453d3d9a2b45a26f115e49ed6e277e18c
Artemy Cheprakov <artemy.cheprakov@gmail.com>
2022-07-26 13:42:22 +0300

    initial commit

---
+++ pair_testing.py
@@ -0,0 +1,74 @@
+'''
+Алгоритм парного тестирования
+'''
+
+import itertools
+import collections
+
+
+data = list(itertools.product(*[(0, 1, 2), (0, 1), (0, 1)]))
+combinations = [tuple(itertools.combinations(c, 2)) for c in data]
+
+pairs_repeat = []
+
+
+for row in list(map(tuple, zip(*combinations))):
+    d = collections.defaultdict(set)
+    for i, _ in enumerate(row):
+        d[row[i]].add(i)
+    pairs_repeat.append(d)
+
+
+not_visited = list(range(len(combinations)))
+visited = set()
+result = set()
+
+
+while len(visited) + len(result) < len(not_visited):
+    ind = not_visited[min(filter(lambda x: x not in (list(visited) + list(result)), not_visited))]
+    result.add(ind)
+    nv_indexes = list(filter(lambda x: x not in (list(visited) + list(result)), not_visited))
+
+    # print(ind)
+    # print(list(nv_indexes))
+
+    redurant = set()
+
+    # step 1
+    for i in nv_indexes:
+        for j in range(len(combinations[i])):
+            # if ind in pairs_repeat[j][combinations[ind][j]]:
+            #     pairs_repeat[j][combinations[ind][j]].remove(ind)
+            # print((i, j), pairs_repeat[j][combinations[ind][j]])
+            # pairs_repeat[j][combinations[ind][j]].remove(ind)
+            # if len(pairs_repeat[j][combinations[ind][j]]) == 1:
+            #     pass
+
+            if combinations[i][j] == combinations[ind][j]:
+                visited.add(i)
+                for k in pairs_repeat[j][combinations[i][j]]:
+                    redurant.add(k)
+
+            # if len(pairs_repeat[j][combinations[i][j]]) == 1:
+            #     solo_index = pairs_repeat[j][combinations[i][j]][0]
+            #     result.add(solo_index)
+
+    redurant_ = set()
+
+    # step 2
+    for d in pairs_repeat:
+        for k, v in d.items():
+            d[k] = v - redurant
+
+            if len(d[k]) == 1:
+                redurant_.add(list(d[k]).pop())
+
+        for k in list(d):
+            if len(d[k]) == 0:
+                del d[k]
+
+    #ste
+
+
+
+print(*[data[i] for i in result], sep='\n')
